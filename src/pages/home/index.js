0fa91ab57165df3fdc11a9093b307f84840fe34e
RoResende <rodrigo.sresende@gmail.com>
2020-09-21 20:09:58 -0300

    Add

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-const home = () => {
+import api from '../../services/api'
+
+const Home = () => {
+
+    const [lists, setLists] = useState([]);
+
+   const handleAddRepository = async () => {
+
+       // e.preventDefault();
+    try {
+        const response = await api.get('/produto');
+        const list = response.data;
+        
+        setLists([list]);
+
+   } catch (error) {
+        alert('Erro na busca por este repositório')
+   }
+   };
+
+   console.log(lists);
+
     return (
         <h1>home</h1>
+        
     );
 };
 
-export default home;
+export default Home;
