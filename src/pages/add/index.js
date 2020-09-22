bdeacd96967bdef238bcf0f5e9305c115896552a
ErculesM <erculesm10@gmail.com>
2020-09-22 19:29:57 -0300

    AAAAAAAAAAAAAAAAAA

--- src/pages/add/index.js
+++ src/pages/add/index.js
@@ -10,6 +10,12 @@ import {
     ContainerPrice,
     ContainerDescription,
     ContainerAdd,
+    ContainerAddInformation,
+    ContainerAddCatSto,
+    ContainerAddCategory,
+    ContainerAddStock,
+    ContainerSpace,
+    Button,
 } from './style.js'
 
 const Add = () => {
@@ -26,8 +32,8 @@ const Add = () => {
         nome: '',
         nomeCategoria: '',
         nomeFuncionario: null,
-        qtdEstoque: 0,
-        valor: 0
+        qtdEstoque: null,
+        valor: null
     });
 
     useEffect(() => {
@@ -79,26 +85,31 @@ const Add = () => {
                 <ContainerImage src='' alt='foto' />
                 <ContainerInformation>
                     <ContainerName>{produto.nome}</ContainerName>
-                    <ContainerPrice>{produto.valor}</ContainerPrice>
+                    <ContainerPrice>R$ {produto.valor}</ContainerPrice>
                     <ContainerDescription>{produto.descricao}</ContainerDescription>
                 </ContainerInformation>
             </ContainerProduct>
-            <div style={{ height: '50px' }}></div>
             <form onSubmit={handleSubmit} >
                 <ContainerAdd>
-                    <div>
-                        <input type='text' placeholder='Name Product' value={produto.nome} onChange={e => setProduto({ ...produto, nome: e.target.value })} />
-                    </div>
-                    <div>
-                        <input type='number' placeholder='Price' value={produto.valor} onChange={e => setProduto({ ...produto, valor: parseFloat(e.target.value) })} />
-                    </div>
-                    <div>
-                        <input type='text' placeholder='Description' value={produto.description} onChange={e => setProduto({ ...produto, descricao: e.target.value })} />
-                    </div>
-                    <div>
+                    <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/10127049-cd7b-45bf-8f09-10742b7e5418/d31ln8w-cb91a8bb-dfd2-4a1f-9b2c-b68bfa613fb9.png/v1/fill/w_900,h_563,q_80,strp/minimalist_pokewall_umbreon_by_thermalsensor_d31ln8w-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD01NjMiLCJwYXRoIjoiXC9mXC8xMDEyNzA0OS1jZDdiLTQ1YmYtOGYwOS0xMDc0MmI3ZTU0MThcL2QzMWxuOHctY2I5MWE4YmItZGZkMi00YTFmLTliMmMtYjY4YmZhNjEzZmI5LnBuZyIsIndpZHRoIjoiPD05MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.eKxbyDmzMD3_kSTlbruZtkHDmAa-JGEouJ32DGiotT4' alt='' />
+                    <ContainerAddInformation>
+                        <div>
+                            <input type='text' placeholder='Name Product' value={produto.nome} onChange={e => setProduto({ ...produto, nome: e.target.value })} />
+                        </div>
+                        <div>
+                            <input type='number' placeholder='Price' value={produto.valor} onChange={e => setProduto({ ...produto, valor: parseFloat(e.target.value) })} />
+                        </div>
+                        <div>
+                            <input type='text' placeholder='Description' value={produto.description} onChange={e => setProduto({ ...produto, descricao: e.target.value })} />
+                        </div>
+                    </ContainerAddInformation>
+                </ContainerAdd>
+                <ContainerAddCatSto>
+                    <ContainerAddStock>
                         <input type='number' placeholder='Stock' value={produto.qtdEstoque} onChange={e => setProduto({ ...produto, qtdEstoque: parseInt(e.target.value) })} />
-                    </div>
-                    <div>
+                    </ContainerAddStock>
+                    <ContainerSpace />
+                    <ContainerAddCategory>
                         <select placeholder="Categoria..." name="dropdown" onChange={e => setProduto({
                             ...produto,
                             idCategoria: parseInt(e.target.value), nomeCategoria: findCategoria(e.target.value)
@@ -108,12 +119,10 @@ const Add = () => {
                                     <option id={cat.id} key={cat.id} value={cat.id}>{cat.nome}</option>)
                             })};
                         </select>
-                    </div>
-                    <div>
-                        <button >Submit</button>
-                    </div>
-                </ContainerAdd>
+                    </ContainerAddCategory>
+                </ContainerAddCatSto>
             </form>
+            <Button><button>Submit</button></Button>
         </>
     );
 };
