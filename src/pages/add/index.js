2b49029b349d40f86a97af2743aa1d2ea7e1ae14
RoResende <rodrigo.sresende@gmail.com>
2020-09-22 10:52:34 -0300

    hooks

--- src/pages/add/index.js
+++ src/pages/add/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const add = () => {
+import api from '../../services/api';
+
+const Add = () => {
+
+    const [produto, setProduto] = useState({
+        dataFabricacao: '',
+        descricao: '',
+        fotoLink: '',
+        id: 0,
+        idCategoria: 0,
+        idFuncionario: 0,
+        nome: '',
+        nomeCategoria: '',
+        nomeFuncionario: '',
+        qtdEstoque: 0,
+        valor: 0
+    });
+
+
+
+    useEffect(() => {
+
+        const handleAddProduct = async () => {
+            try {
+                await api.post('/produto', produto);
+
+            } catch (error) {
+                alert('Erro no acesso a API');
+            }
+        }
+
+        handleAddProduct();
+    }, [precisaFazerEvento]);
+
+    const updateInput = () => {
+        setProduto(props);
+    }
+
+    const handleSubmit = () => {
+        console.log(produto);
+    }
+
     return (
         <h1>add</h1>
     );
 };
 
-export default add;
+export default Add;
